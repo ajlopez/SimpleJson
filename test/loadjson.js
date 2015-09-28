6f3f29b71100c7bafb733cfa0eeffc66346e5400
ajlopez <webmaster@ajlopez.com>
2015-09-28 07:13:01 -0300

    Load json file without extension

--- test/loadjson.js
+++ test/loadjson.js
@@ -8,3 +8,10 @@ exports['load json file'] = function (test) {
     test.deepEqual(json, { name: 'Adam', age: 800 });
 };
 
+exports['load json file without extension'] = function (test) {
+    var json = sj.load('test/jsons/json2');
+    
+    test.ok(json);
+    test.deepEqual(json, { name: 'Eve', age: 700 });
+};
+
