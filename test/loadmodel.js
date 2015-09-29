d004740eb7204f86f8f25b03f54305df1cae6c65
ajlopez <webmaster@ajlopez.com>
2015-09-29 06:43:31 -0300

    Load model with array

--- test/loadmodel.js
+++ test/loadmodel.js
@@ -11,3 +11,17 @@ exports['load simple model'] = function (test) {
     test.equal(model.age, 800);
 }
 
+exports['load model with array'] = function (test) {
+    var model = sj.load('test/model');
+    
+    test.ok(model);
+    test.ok(model.persons);
+    test.ok(Array.isArray(model.persons));
+    test.ok(model.persons.length);
+    test.equal(model.persons.length, 2);
+    test.equal(model.persons[0].name, "Adam");
+    test.equal(model.persons[0].age, 800);
+    test.equal(model.persons[1].name, "Eve");
+    test.equal(model.persons[1].age, 700);
+}
+
